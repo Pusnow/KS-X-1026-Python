0914bca0fc6552ed13d1c97449361e40c6f67de0
YoonWonsup <pusnow@yonsei.ac.kr>
2016-08-22 22:50:06 +0900

    Common Constants

---
+++ ksx1026/__init__.py
@@ -0,0 +1,13 @@
+
+# Common Constants
+
+SBase = ord('\uAC00')
+LBase = ord('\u1100')
+VBase = ord('\u1161')
+TBase = ord('\u11A7')
+LCount = 19
+VCount = 21
+TCount = 28
+TCountAll = 83
+NCount = VCount * TCount  # 588
+SCount = LCount * NCount  # 11172
