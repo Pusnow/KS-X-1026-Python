5146111da648422fbf0350721bf11945d9c1c552
Wonsup Yoon <pusnow@yonsei.ac.kr>
2016-08-23 16:03:05 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(name='KS X 1026 Python',
+      version='0.1',
+      description='Python Implementation of KS X 1026 ',
+      author='Wonsup Yoon',
+      author_email='pusnow@yonsei.ac.kr',
+      url='https://github.com/Pusnow/KS-X-1026-Python',
+      packages=find_packages(),
+     )
